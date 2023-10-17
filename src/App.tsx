52661bb58ff0ec945cad3e7b7046e5f6f6b277c4
adrianjose01 <dejesuseusebbio21@hotmail.com>
2023-10-16 23:58:38 -0400

    v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "../src/app.css";
+import Counter from "./components/Counter";
+import Light from "./components/Light";
 
-function App() {
+type LightTypes = "red" | "green" | "yellow";
+
+const App = () => {
+  const [counter, setCounter] = useState<number>(10);
+  const [light, setLight] = useState<LightTypes>("red");
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((prevCount) => prevCount - 1);
+    }, 1000);
+  }, []);
+
+  const changeLighthandler = (light: LightTypes, count: number) => {
+    setLight(light);
+    setCounter(count);
+  };
+
+  useEffect(() => {
+    if (counter >= 0) return;
+
+    switch (light) {
+      case "red":
+        changeLighthandler("green", 5);
+        break;
+      case "green":
+        changeLighthandler("yellow", 2);
+        break;
+      case "yellow":
+        changeLighthandler("red", 10);
+        break;
+    }
+  }, [counter, light]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Traffic Light</h1>
+      <Counter count={counter} />
+      <Light light={light} />
     </div>
   );
-}
+};
 
 export default App;
